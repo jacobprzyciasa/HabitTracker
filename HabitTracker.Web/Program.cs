fe41937a88ddb0d066294def8cd772055f06e4f9
jacobprzyciasa <kubaprzyciasa@gmail.com>
2024-01-10 10:00:43 +0100

    Update version

--- HabitTracker.Web/Program.cs
+++ HabitTracker.Web/Program.cs
@@ -1,11 +1,14 @@
 using HabitTracker.Web;
+using HabitTracker.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRefitClient<IUserService>().ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7161"));
 
 await builder.Build().RunAsync();
---
+++ HabitTracker.Web/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using HabitTracker.Shared.DataTransferObjects;
+using Refit;
+
+namespace HabitTracker.Web.Services
+{
+    public interface IUserService
+    {
+        [Post("/autentication")]
+        Task UserRegister(UserForRegistrationDto user);
+        [Post("/autentication/login")]
+        Task LoginUser(UserForAuthenticationDto user);
+    }
+}
